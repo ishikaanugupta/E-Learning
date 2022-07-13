14702652f106a0944ded99dcc35873cbf2b369b2
ishikaanugupta <62738947+ishikaanugupta@users.noreply.github.com>
2022-07-13 18:36:24 +0530

    Adding

---
+++ static/js/main.js
@@ -0,0 +1,32 @@
+const date = new Date();
+document.querySelector('.year').innerHTML = date.getFullYear();
+
+setTimeout(function() {
+    $('#message').fadeOut('slow');
+}, 3000);
+
+const accordionItemHeaders = document.querySelectorAll(".accordion-item-header");
+
+accordionItemHeaders.forEach(accordionItemHeader => {
+  accordionItemHeader.addEventListener("click", event => {
+    
+    
+     const currentlyActiveAccordionItemHeader = document.querySelector(".accordion-item-header.active");
+     if(currentlyActiveAccordionItemHeader && currentlyActiveAccordionItemHeader!==accordionItemHeader) {
+       currentlyActiveAccordionItemHeader.classList.toggle("active");
+       currentlyActiveAccordionItemHeader.nextElementSibling.style.maxHeight = 0;
+     }
+
+    accordionItemHeader.classList.toggle("active");
+    const accordionItemBody = accordionItemHeader.nextElementSibling;
+    if(accordionItemHeader.classList.contains("active")) {
+      accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+    }
+    else {
+      accordionItemBody.style.maxHeight = 0;
+    }
+    
+  });
+});
+
+
